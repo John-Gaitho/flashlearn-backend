1ef393c3833da731fd279adbb1f6a5485f7eeb96
John Gaitho <jgaitho016@gmail.com>
2025-03-06 12:59:24 +0300

    deloyed db

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
     """Base configuration class."""
-    SQLALCHEMY_DATABASE_URI = "sqlite:///flashlearn.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://learn_db_60lp_user:jhxk4lcw9nD2QnrjquV8sf9fboTmdnPs@dpg-cv4mijlds78s73e0osj0-a.oregon-postgres.render.com/learn_db_60lp")
+
+    #SQLALCHEMY_DATABASE_URI = os.getenv("postgresql://learn_db_60lp_user:jhxk4lcw9nD2QnrjquV8sf9fboTmdnPs@dpg-cv4mijlds78s73e0osj0-a.oregon-postgres.render.com/learn_db_60lp", )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_secret_key")
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)  # Token expires in 8 hours
-    CORS_RESOURCES = {r"/*": {"origins": "*"}}  # Allow all origins
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=10)  
+    CORS_RESOURCES = {r"/": {"origins": ""}}
 
 
 # Flask Extensions (initialized without app to enable factory pattern)
